f79c24c9808c5fc068b9a46eed6f3f669b9dcdeb
Penelope <freyalabram@gmail.com>
2023-10-04 11:47:31 +0200

    again·

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ mongoose.set("strict", true)
 mongoose.set("strictQuery", false)
 mongoose.set("strictPopulate", true)
 
-console.log("hello")
+console.log("hello test")
 console.log(process.env.TEST)
 mongoose
 .connect(process.env.MONGO_URL, {useUnifiedTopology:true,
