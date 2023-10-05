b01950d52f8e292bb380302454ff2d2183cd8fd6
Penelope <freyalabram@gmail.com>
2023-10-05 21:29:29 +0200

    cont prod debugging

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -1,7 +1,9 @@
 var jwt = require('jsonwebtoken')
 
 const signToken = (payload) => {
+  console.log("check1", payload)
   const token = jwt.sign(payload, process.env.JWT_TOKEN_SECRET, {expiresIn: 24 * 60 * 60})
+  console.log("check2", process.env.JWT_TOKEN_SECRET)
   return token
 }
 
